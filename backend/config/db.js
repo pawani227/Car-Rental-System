e7dcf1c4e1a50872e2eb8af00a0990d51891219f
pawani227 <Pawanihewa90@gmail.com>
2026-05-03 10:28:27 +0530

    update MongoDB connection success message

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect("mongodb://127.0.0.1:27017/rent_db");
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB Connected Successful: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ DB Connection Error: ${error.message}`);
     process.exit(1); // stop server if DB fails
